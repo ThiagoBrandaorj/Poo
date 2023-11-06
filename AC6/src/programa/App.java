ba4a8e233436a44889f084f625a71e29f1dab1a9
Thiago <thiagocbrandao2003@gmail.com>
2023-11-06 10:37:24 -0300

    txt concertado

--- AC6/src/programa/App.java
+++ AC6/src/programa/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Gestor seletor = new Gestor("Alunos.txt");
         String opcao = "0";
-
+        
         while (!opcao.equals("")) {
             seletor.exibeOpcoes();
             opcao = seletor.processaOpcoes();
